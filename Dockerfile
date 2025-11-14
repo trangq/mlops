ea9973c166bb7b19144f9a430ea91cf95d4117db
Trangqh <trangqh1712@gmail.com>
2025-11-14 22:19:32 +0700

    unit test model

---
+++ Dockerfile
@@ -0,0 +1,16 @@
+FROM python:3.12-slim
+
+WORKDIR /app
+
+COPY requirements.txt .
+
+# build normal model & copy to this dockerfile (not recommended)
+
+RUN pip install -r requirements.txt
+
+COPY scripts/ scripts/
+
+ENV PYTHONPATH=/app
+EXPOSE 8080
+
+CMD ["python3", "scripts/session_3/api.py"]
--- tests/session_3/test_api.py
+++ tests/session_3/test_api.py
@@ -1,13 +1,6 @@
-# import pytest
-# from fastapi.testclient import TestClient
-# from scripts.session_3.api import app
-
-# client = TestClient(app)
-
-# def test_root():
-#   response = client.get("/")
-#   assert response.status_code == 200
-#   assert response.json() == {"message": "Hello, FastAPI!"}
+import os
+import sys
+sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '../../')))
 
 import pytest
 from fastapi.testclient import TestClient
